16fcb66ee3cd37d6893b266b047847d89f6b917b
hussain0613 <hussain0613@gmail.com>
2022-02-23 11:44:33 +0600

    added app entrypoint

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+if(session_status() != 2) session_start();
+
+$script_dir = dirname(__FILE__);
+require_once $script_dir."/utils/utils.php";
+require_once $script_dir."/utils/db_connector.php";
+require_once $script_dir."/utils/auth.php";
+
+$conn = DBConnector::get_connection(get_config());
+$user = get_current_app_user($conn);
+$conn->close();
+if($user){
+    header("Location: public/home.php");
+    exit();
+}else{
+    header("Location: public/auth.php");
+    exit();
+}
+?>
